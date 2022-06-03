01cbef176dcddeb0c84470ad7480989f1f052ec1
sivaji <pandasivaji@gmail.com>
2022-06-03 17:57:50 +0530

    changed

--- MAC_project/src/redux/store.js
+++ MAC_project/src/redux/store.js
@@ -1,9 +1,11 @@
 import {reducer} from "./cart/reducer"
 import { itemreducer,selectreducer } from "./items/reducer"
 import { combineReducers,applyMiddleware,compose } from "redux"
-const rootreducer=combineReducers({cart:reducer,items:itemreducer,single:selectreducer})
+// const rootreducer=combineReducers({cart:reducer,items:itemreducer,single:selectreducer})
 import {createStore} from "redux"
 import thunk from 'redux-thunk'
-export const store=createStore(rootreducer,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const rootreducer=combineReducers({cart:reducer,items:itemreducer,single:selectreducer})
+// export const store=createStore(rootreducer,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+export const store=createStore(rootreducer,applyMiddleware(thunk))
 
 console.log(store.getState())
